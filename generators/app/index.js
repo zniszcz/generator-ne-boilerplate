c66efbd3f9bf7d0a92502bd47b175487ec92b30f
Adrian Zniszczyński <a.zniszczynski@nowaera.pl>
2019-02-05 16:48:34 +0100

    Simplyfy generator function

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,35 +1,10 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Jestem wywołaniem dodatkowego polecenia`)
-    );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
   writing() {
 
-    this.log('This is a secret tool well use later');
-
-    const files = [
+    this.files = [
       './config/',
       './src/',
       './.babelrc',
@@ -42,7 +17,7 @@ module.exports = class extends Generator {
       './README.md'
     ];
 
-    files.forEach((path) => this.fs.copy(
+    this.files.forEach((path) => this.fs.copy(
       this.templatePath(path),
       this.destinationPath(path))
     );
