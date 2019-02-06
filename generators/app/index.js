25c75f010cc737cde8b2b7cc3d0f869070a63860
Marcin Maciejewski <m.maciejewski@nowaera.pl>
2019-02-06 15:36:01 +0100

    [WIP] Add prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,79 @@
 
 const Generator = require('yeoman-generator');
+const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
+
+    prompting() {
+      const questions = [
+        {
+          type: 'rawlist',
+          name: 'boilerplateType',
+          message: 'What would you like to generate?',
+          choices: [
+            'Static',
+            'Vue'
+          ],
+          default: 'Static'
+        },
+        {
+          type: 'confirm',
+          name: 'vueMode',
+          message: 'Do you want SSR?',
+          default: true,
+          when: function(answers) {
+
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'unitTests',
+          message: 'Do you want unit tests (Jest)?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'editorconfig',
+          message: 'Do you want editorconfig file?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'mockServer',
+          message: 'Do you want mock server?',
+          default: true
+        },
+      ];
+
+      inquirer
+        .prompt(questions)
+        .then((answers) => {
+          this.log(answers);
+          // this.log(JSON.stringify(answers, null, '  '));
+        });
+    }
+
     writing() {
+        // this.files = [
+        //     './config/',
+        //     './src/',
+        //     './.babelrc',
+        //     './.editorconfig',
+        //     './.eslintrc',
+        //     './.gitignore',
+        //     './.nvmrc',
+        //     './.sass-lint.yml',
+        //     './package.json',
+        //     './README.md',
+        // ];
 
-        this.files = [
-            './config/',
-            './src/',
-            './.babelrc',
-            './.editorconfig',
-            './.eslintrc',
-            './.gitignore',
-            './.nvmrc',
-            './.sass-lint.yml',
-            './package.json',
-            './README.md',
-        ];
-
-        this.files.forEach((path) => this.fs.copy(
-            this.templatePath(path),
-            this.destinationPath(path))
-        );
+        // this.files.forEach((path) => this.fs.copy(
+        //     this.templatePath(path),
+        //     this.destinationPath(path))
+        // );
     }
 
     install() {
-        this.npmInstall();
+        // this.npmInstall();
     }
 };
