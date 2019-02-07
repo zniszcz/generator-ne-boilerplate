c281e66247d5b1e21198c0069d4adbaf10c1d808
Marcin Maciejewski <m.maciejewski@nowaera.pl>
2019-02-07 14:27:07 +0100

    [WIP] Handle prompt options in generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,77 +1,84 @@
 
 const Generator = require('yeoman-generator');
-const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
+  async prompting() {
+    const questions = [
+      {
+        type: 'rawlist',
+        name: 'boilerplateType',
+        message: 'What would you like to generate?',
+        choices: [
+          'Static',
+          'Vue'
+        ],
+        default: 'Static'
+      },
+      {
+        type: 'confirm',
+        name: 'vueMode',
+        message: 'Do you want SSR?',
+        default: true,
+        when: (answers) => answers.boilerplateType === 'Vue'
+      },
+      {
+        type: 'confirm',
+        name: 'unitTests',
+        message: 'Do you want unit tests (Jest)?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'editorconfig',
+        message: 'Do you want editorconfig file?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'mockServer',
+        message: 'Do you want mock server?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'installDependencies',
+        message: 'Do you want generator to install dependencies?',
+        default: true
+      },
+    ];
 
-    prompting() {
-      const questions = [
-        {
-          type: 'rawlist',
-          name: 'boilerplateType',
-          message: 'What would you like to generate?',
-          choices: [
-            'Static',
-            'Vue'
-          ],
-          default: 'Static'
-        },
-        {
-          type: 'confirm',
-          name: 'vueMode',
-          message: 'Do you want SSR?',
-          default: true,
-          when: (answers) => answers.boilerplateType === 'Vue'
-        },
-        {
-          type: 'confirm',
-          name: 'unitTests',
-          message: 'Do you want unit tests (Jest)?',
-          default: true
-        },
-        {
-          type: 'confirm',
-          name: 'editorconfig',
-          message: 'Do you want editorconfig file?',
-          default: true
-        },
-        {
-          type: 'confirm',
-          name: 'mockServer',
-          message: 'Do you want mock server?',
-          default: true
-        },
-      ];
+    await this.prompt(questions)
+      .then((answers) => {
+        this.promptOptions = answers;
+        this.log(this.promptOptions);
+      });
+  }
 
-      inquirer
-        .prompt(questions)
-        .then((answers) => {
-          this.log(answers);
-          // this.log(JSON.stringify(answers, null, '  '));
-        });
+  writing() {
+    // this.files = [
+    //     './config/',
+    //     './src/',
+    //     './.babelrc',
+    //     './.editorconfig',
+    //     './.eslintrc',
+    //     './.gitignore',
+    //     './.nvmrc',
+    //     './.sass-lint.yml',
+    //     './package.json',
+    //     './README.md',
+    // ];
+
+    // this.files.forEach((path) => this.fs.copy(
+    //     this.templatePath(path),
+    //     this.destinationPath(path))
+    // );
+  }
+
+  install() {
+    if (this.promptOptions.installDependencies) {
+      this.npmInstall();
+    } else {
+      this.log('To install your dependencies manually, run: npm install');
     }
-
-    writing() {
-        // this.files = [
-        //     './config/',
-        //     './src/',
-        //     './.babelrc',
-        //     './.editorconfig',
-        //     './.eslintrc',
-        //     './.gitignore',
-        //     './.nvmrc',
-        //     './.sass-lint.yml',
-        //     './package.json',
-        //     './README.md',
-        // ];
-
-        // this.files.forEach((path) => this.fs.copy(
-        //     this.templatePath(path),
-        //     this.destinationPath(path))
-        // );
-    }
-
-    install() {
-        // this.npmInstall();
-    }
+  }
 };
